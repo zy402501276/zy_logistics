e5011130e933be228a04d89da9d84dca123e07db
zhangye <zhangy@pcbdoor.com>
2017-12-04 17:14:49 +0800

    sss

--- logistics.sql
+++ logistics.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
+Source Server         : apache
 Source Server Version : 50505
 Source Host           : localhost:3306
 Source Database       : logistics
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-12-04 01:08:01
+Date: 2017-12-04 17:14:38
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -32,6 +32,25 @@ CREATE TABLE `distribute` (
 -- ----------------------------
 -- Records of distribute
 -- ----------------------------
+
+-- ----------------------------
+-- Table structure for `driveraddress`
+-- ----------------------------
+DROP TABLE IF EXISTS `driveraddress`;
+CREATE TABLE `driveraddress` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '司机位置表主键',
+  `longitude` varchar(20) DEFAULT NULL COMMENT '经度',
+  `latitude` varchar(20) DEFAULT NULL COMMENT '纬度',
+  `driverId` int(11) unsigned DEFAULT NULL COMMENT '司机',
+  `createTime` int(40) unsigned DEFAULT NULL COMMENT '新增时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='司机位置表';
+
+-- ----------------------------
+-- Records of driveraddress
+-- ----------------------------
+INSERT INTO `driveraddress` VALUES ('1', '3', '1', '2', '1512369714');
+INSERT INTO `driveraddress` VALUES ('2', '3', '3', '3', '1512369724');
 
 -- ----------------------------
 -- Table structure for `loginlog`
@@ -76,13 +95,15 @@ CREATE TABLE `order` (
   `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态[1正常 | 0失效]',
   `createTime` int(40) unsigned DEFAULT NULL COMMENT '新增时间',
   `updateTime` int(40) unsigned DEFAULT NULL COMMENT '最后更新时间',
+  `driverId` int(11) unsigned DEFAULT NULL COMMENT '司机-关联user表主键',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='订单表';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='订单表';
 
 -- ----------------------------
 -- Records of order
 -- ----------------------------
-INSERT INTO `order` VALUES ('1', '1', '1', '1', '1', '第一笔订单', '80', '600', '123456adbcd', '1', '广东省 深圳市 南山区 中山大学', '广东省 惠州市 CBD 万达广场', '1512318454', '1514736000', '1', '1512105949', '1512105949');
+INSERT INTO `order` VALUES ('1', '1', '1', '1', '1', '第一笔订单', '80', '600', '123456adbcd', '2', '广东省 深圳市 南山区 中山大学', '广东省 惠州市 CBD 万达广场', '1512318454', '1512403200', '1', '1512105949', '1512105949', '1');
+INSERT INTO `order` VALUES ('2', '1', '1', '1', '1', '第二笔订单', '90', '1000', '123123123', '3', '广东省 深圳市 南山区 中山大学', '广东省 惠州市 CBD 万达广场', '1512403300', '1512576000', '1', '1512489600', '1512376295', '2');
 
 -- ----------------------------
 -- Table structure for `ordercharger`
@@ -138,6 +159,25 @@ CREATE TABLE `ordergoods` (
 -- Records of ordergoods
 -- ----------------------------
 INSERT INTO `ordergoods` VALUES ('1', '1', '货物1', '1', '2', '3.0', '4', '100', '1', '1', null, null);
+
+-- ----------------------------
+-- Table structure for `orderimg`
+-- ----------------------------
+DROP TABLE IF EXISTS `orderimg`;
+CREATE TABLE `orderimg` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '图片表id',
+  `img` varchar(60) DEFAULT NULL COMMENT '图片路径',
+  `type` tinyint(3) unsigned DEFAULT NULL COMMENT '类型[1装货 |2 卸货 | 3装货签字 | 4卸货签字]',
+  `orderid` int(11) unsigned DEFAULT NULL COMMENT '订单id',
+  `createTime` int(40) DEFAULT NULL COMMENT '新增时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of orderimg
+-- ----------------------------
+INSERT INTO `orderimg` VALUES ('1', './Upload/20171204/5a2506c16a4d2.jpg1', '1', '1', '1512376071');
+INSERT INTO `orderimg` VALUES ('2', './Upload/20171204/5a2506c16a4d2.jpg', '1', '2', '1512376295');
 
 -- ----------------------------
 -- Table structure for `user`
