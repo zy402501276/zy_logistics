089a005f0556746283d77ee6bb4ad3ec2a3df295
liulin <597852546@qq.com>
2017-12-07 01:21:48 +0800

    解决冲突

--- App/Home/Model/OrdergoodsModel.class.php
+++ App/Home/Model/OrdergoodsModel.class.php
@@ -1,15 +1,40 @@
 <?php
-// +----------------------------------------------------------------------
-// | wuliu.System [ All demangs in it! ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2015 http://www.yoursite.com All rights reserved.
-// +----------------------------------------------------------------------
-// | Author: shigin <597852546@qq.com> <http://shigin.cc>
-// +----------------------------------------------------------------------
 namespace Home\Model;
-/**
- * 货物模型
- */
-class OrdergoodsModel extends BaseModel
-{
+class OrderGoodsModel extends BaseModel{
+    protected $tableName = "ordergoods";
+
+    /*
+     * 获取订单物品数量重量
+     * @author zhangye
+     */
+    public function getGoodsInfo($ordeId){
+        $array = [];
+        $result = $this
+                  ->field('sum(count),sum(goodsWeight)')
+                  ->where("orderId=$ordeId")
+                  ->find();
+        if(!empty($result)){
+            $array = $result;
+        }
+        return $array;
+    }
+    /*
+     * 获取订单物品种类
+     * @author zhangye
+     */
+    public function getGoodsType($ordeId){
+        $string = '';
+        $result = $this
+            ->JOIN("LEFT JOIN `goodstype` ON `ordergoods`.goodsType = `goodstype`.id")
+            ->where("orderId=$ordeId")
+            ->select();
+        if(!empty($result)){
+            foreach ($result as $key =>$value){
+                $string .= $value['name'].'/';
+            }
+            $string = substr($string,0,-1);
+        }
+        return $string;
+    }
+
 }
